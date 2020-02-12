4e6c1d1901b3ab147e599c5e63fc361f59d4f0ce
Harry Williams <h.williams1996@gmail.com>
2020-02-12 14:09:18 +0000

    add while loop example

--- ageCalculator.js
+++ ageCalculator.js
@@ -1,5 +1,3 @@
 function ageCalculator(name, birthYear, currentYear) {
   return name + ' is ' + (currentYear - birthYear) + ' years old.';
 }
-
-console.log(ageCalculator('Harry', 1996, 2019));
---
+++ packing-list.js
@@ -0,0 +1,16 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+// Iterating over an area with a FOR loop
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+
+// Iterating over an array with a WHILE loop
+console.log("Kitchen stuff to pack:");
+let i = 0
+while (i < packingList.length) {
+	console.log(packingList[i]);
+	i++;
+} 
+
